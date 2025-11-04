e213144cb1f7b6e0eb183b8c0d735b09276ad0fa
taamalito2002 <jgutierrezlopez195@gmail.com>
2025-11-04 01:09:03 -0500

    Incio de proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from controlador.main_controller import Controlador
+
+def main():
+    # Inicializamos el controlador
+    controlador = Controlador()
+    
+    # Aqui ira el bucle principal y las interacciones del usuario
+    pass  # Por ahora no hacemos nada
+
+if __name__ == "__main__":
+    main()
